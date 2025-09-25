73fe028b8f3810b0de3f4f50646730c6f6deb497
DylanBlanco <dylanblanco1198@gmail.com>
2025-09-25 17:30:12 +0200

    corretto alphabetMessages.js (funziona solo con un server tipo node)

--- js/script.js
+++ js/script.js
@@ -12,3 +12,35 @@ const viewText = document.getElementById('view-text');
 //         });
 //     }
 // });
+
+
+// alphabetMessages.js
+
+const alphabetMessages = [
+    { letter: 'a', message: 'Amico' },
+    { letter: 'b', message: 'Bella giornata' },
+    { letter: 'c', message: 'Ciao!' },
+    { letter: 'd', message: 'Divertiti' },
+    { letter: 'e', message: 'Energia positiva' },
+    { letter: 'f', message: 'Forza!' },
+    { letter: 'g', message: 'Grazie' },
+    { letter: 'h', message: 'Hai capito?' },
+    { letter: 'i', message: 'Incredibile' },
+    { letter: 'j', message: 'Joy!' },
+    { letter: 'k', message: 'Karaoke time' },
+    { letter: 'l', message: 'Love' },
+    { letter: 'm', message: 'Mangiamo' },
+    { letter: 'n', message: 'Notte serena' },
+    { letter: 'o', message: 'Oggi splendido' },
+    { letter: 'p', message: 'Perfetto' },
+    { letter: 'q', message: 'Quanto costa?' },
+    { letter: 'r', message: 'Rilassati' },
+    { letter: 's', message: 'Salve!' },
+    { letter: 't', message: 'Ti voglio bene' },
+    { letter: 'u', message: 'Un abbraccio' },
+    { letter: 'v', message: 'Viva la vita' },
+    { letter: 'w', message: 'Wow!' },
+    { letter: 'x', message: 'XOXO' },
+    { letter: 'y', message: 'Yes!' },
+    { letter: 'z', message: 'Zero problemi' }
+];
