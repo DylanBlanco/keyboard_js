fdeadc8b63ee5845bff95f30d508fc4db13622cf
DylanBlanco <dylanblanco1198@gmail.com>
2025-09-25 17:18:09 +0200

    file alphabetMessages.js importato in script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
+// Import del file alphabetMessages.js
+import alphabetMessages from './alphabetMessages.js';
+
 const viewText = document.getElementById('view-text');
-const jsButtonQ = document.querySelector('.js-button-q');
-const jsButtonW = document.querySelector('.js-button-w');
-const jsButtonE = document.querySelector('.js-button-e');
-const jsButtonR = document.querySelector('.js-button-r');
-const jsButtonT = document.querySelector('.js-button-t');
-const jsButtonY = document.querySelector('.js-button-y');
 
 jsButtonQ.addEventListener('click', () => {
     viewText.innerHTML += 'Ciao' + ' ';
