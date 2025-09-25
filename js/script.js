fd0731b67b399fa6e7d50ed72c6bc94d4c298542
DylanBlanco <dylanblanco1198@gmail.com>
2025-09-25 16:37:05 +0200

    principio della logica

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const viewText = document.getElementById('view-text');
+const jsButton = document.querySelector('.js-button');
+
+jsButton.addEventListener('click', function consoleCiao() {
+    console.log('ciao');
+});
